6df73452725bbd0a801cbc5ed84f024c5c614a27
subuxd <103200387+subuxd@users.noreply.github.com>
2023-07-18 03:46:56 +0530

    Create App.js

---
+++ client/src/App.js
@@ -0,0 +1,19 @@
+import TextEditor from "./TextEditor";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom"
+import { v4 as uuidV4 } from "uuid"
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Navigate to={`/documents/${uuidV4()}`} />} />
+        <Route path="/documents/:id" element={<TextEditor />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
+
+
+ //uuid is a pkg used to generate unique ID
